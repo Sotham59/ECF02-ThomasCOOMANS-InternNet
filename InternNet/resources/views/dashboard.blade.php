5e81e10362a50f3ca6c1c3a9d4f5a1447b49353f
Thomas <tom.coomans59@gmail.com>
2024-04-24 13:44:21 +0200

    Modifs titre sur dashboard

--- InternNet/resources/views/dashboard.blade.php
+++ InternNet/resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight flex lg:justify-center">
+            {{ __('Bienvenue sur votre espace entreprise') }}
         </h2>
         
     </x-slot>
@@ -10,7 +10,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("Vos offres de stage en ligne: ") }}
                 </div>
             </div>
         </div>
